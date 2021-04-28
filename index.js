319cd33f9592749a0edd40aab8c4457f03eb00f0
G3V <46306494+G3VV@users.noreply.github.com>
2021-04-28 01:30:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const { join } = require("path");
+const fetch = require("node-fetch")
+
+app.use(express.static(join(__dirname, "/")));
+
+app.listen(80, () => {
+  console.log("Online");
+});
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "/index.html"));
+});
