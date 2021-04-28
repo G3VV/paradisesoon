e96597bba502605e5fd164d0e79038207dea5fb3
G3V <46306494+G3VV@users.noreply.github.com>
2021-04-28 18:16:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.listen(80, () => {
   console.log("Online");
 });
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "/index.html"));
+  res.sendFile(join(__dirname, "index.html"));
 });
